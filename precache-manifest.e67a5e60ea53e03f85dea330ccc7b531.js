0ac3ef5af9c379f93f83ab0a074ff45deb5fc9d8
Marlon Leoner <marlon.leoner@gmail.com>
2020-02-18 12:36:56 -0300

    Updates

---
+++ precache-manifest.e67a5e60ea53e03f85dea330ccc7b531.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309cdadeda9ab89b1545da671c58f7dc",
+    "url": "/sim/index.html"
+  },
+  {
+    "revision": "d979c0091043ef378b80",
+    "url": "/sim/static/js/2.310df694.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/sim/static/js/2.310df694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621aa530059f29ef0fb4",
+    "url": "/sim/static/js/main.0ae27eab.chunk.js"
+  },
+  {
+    "revision": "d3b813350dee37fcd22a",
+    "url": "/sim/static/js/runtime-main.cc13dbc5.js"
+  },
+  {
+    "revision": "b41a54879a946ae5d432cce42730d60c",
+    "url": "/sim/static/media/logo_inep.b41a5487.png"
+  },
+  {
+    "revision": "350d0aaec37c6ecb4ff8b20c2ad03271",
+    "url": "/sim/static/media/logo_petcc.350d0aae.svg"
+  },
+  {
+    "revision": "cd4cd97f22d4fed832dd8bd3f4ea44e9",
+    "url": "/sim/static/media/logo_sbc.cd4cd97f.png"
+  }
+]);
